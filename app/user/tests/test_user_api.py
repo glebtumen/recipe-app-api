f706898b0ca76f1a40eab3dc08a6aa6c660237a3
glebtumen <tyumenchevgleb@mail.ru>
2024-02-01 18:22:51 +0500

    modified:   app/user/tests/test_user_api.py

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -56,7 +56,6 @@ class PublicUserApiTests(TestCase):
             'name': 'Test Name',
         }
 
-        create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
